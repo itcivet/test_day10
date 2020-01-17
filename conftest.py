08ff1353832de86c48aab17c9295eacdbb894451
yyyyy <yyy@163>
2020-01-17 14:47:00 +0800

    添加获取工程根目录绝对路径方法以及使用验证

---
+++ conftest.py
@@ -0,0 +1,4 @@
+import os
+
+"""获取当前文件的上一层文件夹的绝对路径"""
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
--- read_data/read_yaml.py
+++ read_data/read_yaml.py
@@ -1,14 +1,19 @@
 import yaml
 
+from conftest import BASE_DIR
+
 
 def build_login_data():
-
-
-    with open("../data/login_data.yaml", encoding="utf-8")as f:
+    #  with open("../data/login_data.yaml", encoding="utf-8")as f:
+    with open(BASE_DIR + "/data/login_data.yaml", encoding="utf-8")as f:
         data = yaml.safe_load(f)
         data_list = list()
         for i in data:
             data_list.append((i.get("num"),
                               i.get("pwd")))
         print(data_list)
-        return data_list # 返回参数数据
+        return data_list  # 返回参数数据
+
+
+if __name__ == '__main__':
+    print(build_login_data())
